dc608de1f7d7fb78569a7e93d88f6e115a5ddb38
davojan <davojan@gmail.com>
2016-05-19 19:27:58 +0300

    first workable version of backup uploading logic

--- s3-backuper.py
+++ s3-backuper.py
@@ -1,22 +1,59 @@
 #!/usr/bin/env python
 
-from plumbum.cmd import aws, ls
-from plumbum import cli, colors
-import sys
+from sys import stdout
+from os import listdir
+from os.path import isfile, join, getsize, getctime
+import time
+
+from plumbum.cmd import aws, ls, grep, mv, mkdir, rm
+from plumbum import cli
 
 
 s3 = aws["s3"]
 
 class MyApp(cli.Application):
-    PROGNAME = colors.green
+
     VERSION = "0.0.1"
 
     verbose = cli.Flag(["v", "verbose"], help = "If given, I will be very talkative")
+    keepCount = cli.SwitchAttr(["k", "keep"], int, default = 2, help = "Max count of backup files to be kept")
 
-    def main(self, srcDir, dstBucket, dstPath):
-        print(s3("ls"))
+    def main(self, srcDir, dstBucket, dstDir):
+        # protect to prevent deleting of all backups
+        if self.keepCount < 2:
+            self.keepCount = 2
+
+        s3DirPath = "s3://" + dstBucket + "/" + dstDir
         if self.verbose:
-            print("Yadda " * 200)
+            print("Sending backups from", srcDir, "to", s3DirPath, flush = True)
+
+        # check if bucket exists and create if not
+        lines = (s3["ls"] | grep[dstBucket])().splitlines()
+        if not lines:
+            if self.verbose:
+                print("Bucket doesn't exist. Creating...")
+            (s3["mb", "s3://" + dstBucket] > stdout)()
+
+        # create dir for processed backup files (if not exists)
+        processedDir = join(srcDir, "_processed")
+        mkdir("-p", processedDir)
+
+        # process new files
+        for f in listdir(srcDir):
+            fullPath = join(srcDir, f)
+            if isfile(fullPath) and getsize(fullPath) > 0:
+                datePrefix = time.strftime("%Y-%m-%d-", time.localtime(getctime(fullPath)))
+                processedFileName = datePrefix + f
+                mv(fullPath, join(processedDir, processedFileName))
+
+        # remove old backups, keep only requested count (--keep)
+        for f in ls("-c", processedDir).splitlines()[self.keepCount:]:
+            if self.verbose:
+                print("Removing old backup", f, flush = True)
+            rm(join(processedDir, f))
+
+        # sync to s3
+        (s3["sync", processedDir, s3DirPath, "--storage-class", "STANDARD_IA", "--delete"] > stdout)()
 
 
 if __name__ == "__main__":
